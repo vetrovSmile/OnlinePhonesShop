8aac55fe7c757a58b99b09f33c81ec3f2a9d45da
Pavel Egorov <pauil@mail.ru>
2022-08-16 14:39:54 +0300

    Add bottom navigation, color change.

--- app/src/main/java/com/example/onlinephonesshop/presentation/MainActivity.kt
+++ app/src/main/java/com/example/onlinephonesshop/presentation/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.onlinephonesshop.R
 import com.example.onlinephonesshop.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
+
+  //  private lateinit var bottomMenu: BottomNavigationMenuView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +24,10 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+
     }
+
+
+
 }
